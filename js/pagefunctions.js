5b9fcde57b4a823a4ba526e1fcc678f4e919b5d5
wills-web <willmulrooney@gmail.com>
2017-05-23 22:49:46 +0100

    Basics upload
    Adding the base files, along with index and about.

---
+++ js/pagefunctions.js
@@ -0,0 +1,14 @@
+function sayHello(name) {
+		alert("Hello " + name + "!")
+}
+
+/*function whenPageLoaded() {
+	$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    var os = $('#jumbotronbox').offset().top;
+    var ht = $('#jumbotronbox').height();
+    if(scroll > os + ht){
+        $('.nav .navbar-nav').addClass('.blue');
+    }
+	});
+}*/
