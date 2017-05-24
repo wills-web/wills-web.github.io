799a721b5f4689fbce66b0e428ffcb8c75ff5a46
wills-web <willmulrooney@gmail.com>
2017-05-24 12:51:45 +0100

    Updating Index & About
    JavaScript files

--- js/pagefunctions.js
+++ js/pagefunctions.js
@@ -2,6 +2,17 @@ function sayHello(name) {
 		alert("Hello " + name + "!")
 }
 
+function showComingSoon() {
+    // Get the snackbar DIV
+    var x = document.getElementById("snackbar")
+
+    // Add the "show" class to DIV
+    x.className = "show";
+
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
 /*function whenPageLoaded() {
 	$(window).scroll(function() {
     var scroll = $(window).scrollTop();
